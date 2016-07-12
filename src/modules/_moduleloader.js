cc36999de7414f58185ac1752f372a1508618ea0
emyarod <allllaboutyou@gmail.com>
2016-07-12 01:20:47 -0700

    Add module loader

---
+++ src/modules/_moduleloader.js
@@ -0,0 +1,43 @@
+export {
+  greeting
+}
+from './greeting.js';
+
+export {
+  flex
+}
+from './flex.js';
+
+export {
+  twitter
+}
+from './twitter.js';
+
+export {
+  instagram
+}
+from './instagram.js';
+
+export {
+  getSimilarArtists,
+  getArtistInfo,
+  addlfm,
+  nowplaying,
+  getWeeklyCharts
+}
+from './lastfm.js';
+
+export {
+  searchYouTube
+}
+from './youtube.js';
+
+export {
+  textTranslate
+}
+from './translate.js';
+
+export {
+  turntable
+}
+from './turntable.js';
