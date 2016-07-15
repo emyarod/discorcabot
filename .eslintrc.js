8ebae588e8bb06aff6e1695e0fc6a067a5a47940
emyarod <allllaboutyou@gmail.com>
2016-07-14 22:43:28 -0700

    Modify no-console rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-    "extends": "airbnb",
-    "installedESLint": true,
-    "plugins": [
-        "react"
-    ]
+  extends: 'airbnb',
+  installedESLint: true,
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': ['error', {
+      allow: ['warn', 'error'],
+    }],
+  },
 };
