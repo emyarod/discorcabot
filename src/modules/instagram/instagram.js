b4da0e584303471177ad3e10cc64c95a41960740
emyarod <allllaboutyou@gmail.com>
2016-07-14 23:45:34 -0700

    Add descriptive comments

--- src/modules/instagram/instagram.js
+++ src/modules/instagram/instagram.js
@@ -1,14 +1,96 @@
 import request from 'request';
 import cheerio from 'cheerio';
 
+/**
+ * getMediaURL() returns URL of latest Instagram post
+ * @param {Boolean} isVideo Whether medium is image or video
+ * @param {String} userID Instagram user ID
+ * @return {String} mediaURL Direct link to Instagram medium
+ */
+function getMediaURL(isVideo, userID) {
+  // use promise for async request in case media node is a video
+  return new Promise((resolve) => {
+    // scrape video link if the media node is a video
+    if (isVideo) {
+      const {
+        0: {
+          entry_data: {
+            ProfilePage: [{
+              user: {
+                media: {
+                  nodes: [{
+                    code: mediaCode,
+                  }],
+                },
+              },
+            }],
+          },
+        },
+      } = userID;
+
+      // scrape Instagram post page
+      request(`https://www.instagram.com/p/${mediaCode}`, (e, res, html) => {
+        if (!e && res.statusCode === 200) {
+          const $ = cheerio.load(html, {
+            lowerCaseTags: true,
+            xmlMode: true,
+          });
+
+          const {
+            0: {
+              attribs: {
+                content: mediaURL,
+              },
+            },
+          } = $('meta[property="og:video:secure_url"]');
+
+          resolve(mediaURL);
+        }
+      });
+    } else {
+      // extract image URL from JSON object into variable
+      let {
+        0: {
+          entry_data: {
+            ProfilePage: [{
+              user: {
+                media: {
+                  nodes: [{
+                    display_src: mediaURL,
+                  }],
+                },
+              },
+            }],
+          },
+        },
+      } = userID;
+
+      /**
+       * trim ig cache key from URL
+       *   '.*?' matches any character (except newline)
+       *   '(?:\/[\w\.\-]+)+)' matches UNIX Path syntax
+       */
+      [mediaURL] = mediaURL.match(/.*?(?:\/[\w\.\-]+)+/);
+      resolve(mediaURL);
+    }
+  });
+}
+
+/**
+ * instagram() returns the latest Instagram post of a given account
+ * based on user input
+ * @param {Object} orcabot Discord.Client
+ * @param {Object} message represents the data of the input message
+ * @return {Boolean} unnamed Whether request occurred
+ */
 export function instagram(orcabot, message) {
-  const username = message.content.replace('.ig ', '');
+  const user = message.content.replace('.ig ', '');
 
   // scrape profile page
-  request(`https://www.instagram.com/${username}`, (e, res, html) => {
-    if (!e && res.statusCode == 200) {
-      // validate username
-      if (username === '') {
+  request(`https://www.instagram.com/${user}`, (e, res, html) => {
+    if (!e && res.statusCode === 200) {
+      // validate user
+      if (user === '') {
         return;
       }
 
@@ -24,6 +106,8 @@ export function instagram(orcabot, message) {
           // return value of window._sharedData variable as JSON
           return JSON.parse($(element).text().slice(21, -1));
         }
+
+        return 0;
       });
 
       // return if profile is private
@@ -115,86 +199,21 @@ export function instagram(orcabot, message) {
         },
       } = userID;
 
-      function getMediaURL(isVideo) {
-        // use promise for async request in case media node is a video
-        return new Promise((resolve, reject) => {
-          // scrape video link if the media node is a video
-          if (isVideo) {
-            const {
-              0: {
-                entry_data: {
-                  ProfilePage: [{
-                    user: {
-                      media: {
-                        nodes: [{
-                          code: mediaCode,
-                        }],
-                      },
-                    },
-                  }],
-                },
-              },
-            } = userID;
-
-            // scrape Instagram post page
-            request(`https://www.instagram.com/p/${mediaCode}`, (e, res, html) => {
-              if (!e && res.statusCode === 200) {
-                const $ = cheerio.load(html, {
-                  lowerCaseTags: true,
-                  xmlMode: true,
-                });
-
-                const {
-                  0: {
-                    attribs: {
-                      content: mediaURL,
-                    },
-                  },
-                } = $('meta[property="og:video:secure_url"]');
-
-                resolve(mediaURL);
-              }
-            });
-          } else {
-            // extract image URL from JSON object into variable
-            let {
-              0: {
-                entry_data: {
-                  ProfilePage: [{
-                    user: {
-                      media: {
-                        nodes: [{
-                          display_src: mediaURL,
-                        }],
-                      },
-                    },
-                  }],
-                },
-              },
-            } = userID;
-
-            /**
-             * trim ig cache key from URL
-             *   '.*?' matches any character (except newline)
-             *   '(?:\/[\w\.\-]+)+)' matches UNIX Path syntax
-             */
-            [mediaURL] = mediaURL.match(/.*?(?:\/[\w\.\-]+)+/);
-            resolve(mediaURL);
-          }
-        });
-      }
-
-      getMediaURL(isVideo).then((mediaURL) => {
+      // get link to media then reply in chat
+      getMediaURL(isVideo, userID).then((mediaURL) => {
         if (isVideo) {
-          const content = `Most recent Instagram post by **\`@${username}\`:** \`${caption}\`\n${mediaURL}`;
-          orcabot.sendMessage(message, content, (error, message) => {
+          // link video in chat
+          let content = 'Most recent Instagram post by ';
+          content += `**\`@${username}\`:** \`${caption}\`\n${mediaURL}`;
+          orcabot.sendMessage(message, content, (error) => {
             if (error) {
               console.warn(`INSTAGRAM sendMessage (video) -- ${error}`);
             }
           });
         } else {
+          // attach image to reply
           const content = `Most recent Instagram post by **\`@${username}\`:** \`${caption}\``;
-          orcabot.sendFile(message, mediaURL, null, content, (error, message) => {
+          orcabot.sendFile(message, mediaURL, null, content, (error) => {
             if (error) {
               console.warn(`INSTAGRAM sendFile (photo) -- ${error}`);
             }
