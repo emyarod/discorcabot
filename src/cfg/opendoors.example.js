52cca8bbb58623912421480dc451ed6d6045c060
emyarod <allllaboutyou@gmail.com>
2016-07-18 18:00:27 -0700

    Add example keys object

---
+++ src/cfg/opendoors.example.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // Discord
+  discordToken: 'your-discordToken-here',
+  discordClientID: 'your-discordClientID-here',
+  botOwnerID: 'your-botOwnerID-here',
+
+  // Twitter
+  twitterConsumerKey: 'your-twitterConsumerKey-here',
+  twitterConsumerSecret: 'your-twitterConsumerSecret-here',
+  twitterAccessToken: 'your-twitterAccessToken-here',
+  twitterAccessTokenSecret: 'your-twitterAccessTokenSecret-here',
+
+  // Last.fm
+  lfmApiKey: 'your-lfmApiKey-here',
+  lfmSecret: 'your-lfmSecret-here',
+
+  // Google
+  googleCX: 'your-googleCX-here',
+  googleAPIKey: 'your-googleAPIKey-here',
+
+  // Microsoft Translator
+  clientID: 'your-clientID-here',
+  msTranslatorKey: 'your-msTranslatorKey-here',
+
+  // SoundCloud
+  scClientID: 'your-scClientID-here',
+  scClientSecret: 'your-scClientSecret-here',
+  scRedirectURI: 'your-scRedirectURI-here',
+};
