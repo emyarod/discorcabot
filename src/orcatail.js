b9b4091d9c7d5b0258379f1656446d25be48df0f
emyarod <allllaboutyou@gmail.com>
2016-07-18 17:58:01 -0700

    Utilize promises

--- src/orcatail.js
+++ src/orcatail.js
@@ -1,11 +1,10 @@
 // import bot config
 import {
-  orcabot
+  orcabot,
 } from './cfg/config.js';
 
 // import modules
 import {
-  greeting,
   flex,
   twitter,
   instagram,
@@ -16,7 +15,7 @@ import {
   getWeeklyCharts,
   searchYouTube,
   textTranslate,
-  turntable
+  turntable,
 } from './modules/_moduleloader.js';
 
 orcabot.on('message', (message) => {
@@ -26,59 +25,101 @@ orcabot.on('message', (message) => {
 
   // console.log(message);
 
-  // hi
-  greeting(orcabot, message);
-
   // nfz
   flex(orcabot, message);
 
   switch (true) {
-    case message.content.search(/^(\.tw )/gi) === 0:
+    case !message.content.search(/^(\.tw )/gi):
       // Twitter
-      twitter(orcabot, message);
+      twitter(message).then((response) => {
+        orcabot.reply(message, response);
+      }, (error) => {
+        orcabot.reply(message, error);
+      });
       break;
 
-    case message.content.search(/^(\.ig )/gi) === 0:
+    case !message.content.search(/^(\.ig )/gi):
       // Instagram
-      instagram(orcabot, message);
+      instagram(message).then((response) => {
+        const [responseType, mediaURL, content] = response;
+        if (responseType === 'video') {
+          // link video in chat
+          orcabot.sendMessage(message, content, (error) => {
+            if (error) {
+              console.warn(`INSTAGRAM sendMessage (video) -- ${error}`);
+            }
+          });
+        } else {
+          // attach image to reply
+          orcabot.sendFile(message, mediaURL, null, content, (error) => {
+            if (error) {
+              console.warn(`INSTAGRAM sendFile (photo) -- ${error}`);
+            }
+          });
+        }
+      }, (error) => {
+        orcabot.reply(message, error);
+      });
       break;
 
-    case message.content.search(/^(\.similar )/gi) === 0:
+    case !message.content.search(/^(\.similar )/gi):
       // Last.fm get similar artists
-      getSimilarArtists(orcabot, message);
+      getSimilarArtists(message).then((response) => {
+        orcabot.reply(message, response);
+      }, (error) => {
+        orcabot.reply(message, error);
+      });
       break;
 
-    case message.content.search(/^(\.getinfo )/gi) === 0:
+    case !message.content.search(/^(\.getinfo )/gi):
       // Last.fm get artist info
-      getArtistInfo(orcabot, message);
+      getArtistInfo(message).then((response) => {
+        const [image, content] = response;
+        if (image !== null) {
+          // attach image if Last.fm returns an image
+          orcabot.sendFile(message, image, null, content, (error) => {
+            if (error) {
+              console.warn(`INSTAGRAM sendFile (photo) -- ${error}`);
+            }
+          });
+        } else {
+          orcabot.reply(message, content);
+        }
+      }, (error) => {
+        orcabot.reply(message, error);
+      });
       break;
 
-    case message.content.search(/^(\.addlfm )/gi) === 0:
+    case !message.content.search(/^(\.addlfm )/gi):
       // Last.fm add lfm username to db
       addlfm(orcabot, message);
       break;
 
-    case message.content.search(/^(\.np)/gi) === 0:
+    case !message.content.search(/^(\.np)/gi):
       // Last.fm now playing .np <self/user/registered handle>
       nowplaying(orcabot, message);
       break;
 
-    case message.content.search(/^(\.charts)/gi) === 0:
+    case !message.content.search(/^(\.charts)/gi):
       // Last.fm weekly charts .charts <self/user/registered handle>
       getWeeklyCharts(orcabot, message);
       break;
 
-    case message.content.search(/^(\.yt )/gi) === 0 && message.content !== '.yt ':
+    case !message.content.search(/^(\.yt )/gi) && message.content !== '.yt ':
       // YouTube
-      searchYouTube(orcabot, message);
+      searchYouTube(message).then((response) => {
+        orcabot.reply(message, response);
+      }, (error) => {
+        orcabot.reply(message, error);
+      });
       break;
 
-    case message.content.search(/^(\.tr )/gi) === 0:
+    case !message.content.search(/^(\.tr )/gi):
       // translator
       textTranslate(orcabot, message);
       break;
 
-    case message.content.search(/^(\.dj )/gi) === 0 && message.content !== '.dj ':
+    case !message.content.search(/^(\.dj )/gi) && message.content !== '.dj ':
       // turntable
       turntable(orcabot, message);
       break;
