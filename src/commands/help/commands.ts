9527122705ef247382642af0f3012d0561142f04
emyarod <afw5059@gmail.com>
2018-08-27 21:16:04 -0500

    Update `.help charts` output

--- src/commands/help/commands.ts
+++ src/commands/help/commands.ts
@@ -5,7 +5,7 @@ export default {
   ig: `**Instagram module!** Usage: .ig <username> will return this user's most recent photo/video, along with the caption (if applicable).`,
   // 'Last.fm'
   addlfm: `**Last.fm module!** Usage: \`.addlfm <username>\` stores your Discord ID and Last.fm username in the bot's database for usage with the \`.np\` and \`.charts\` commands.`,
-  charts: `**Last.fm module!** Usage: \`.charts\` (with no other parameters) returns your top five most played artists in the last seven days on Last.fm (you must be in the bot's database for this function to work!). Entering .charts <username> returns this data for the provided user on Last.fm.`,
+  charts: `**Last.fm module!** Usage: \`.charts\` (with no other parameters) returns your top five most played artists in the last seven days on Last.fm (you must be in the bot's database for this function to work!). Entering \`.charts <username>\` returns this data for the provided user on Last.fm.`,
   getinfo: `**Last.fm module!** Usage: \`.getinfo <artist>\` returns a short description of the given artist`,
   np: `**Last.fm module!** Usage: \`.np [@Discord user | Last.fm handle]\` returns the currently playing or most recently scrobbled track on the associated Last.fm account. If no argument is provided, the bot will look up its local database to see if the command caller is linked to a Last.fm account, and then fetch the data from Last.fm.`,
   similar: `**Last.fm module!** Usage: \`.similar <artist>\` returns a list of similar artists as well as a percentage value of how closely the artists match, according to Last.fm.`,
