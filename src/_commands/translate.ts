ff02cf4d2014216a26ecdafaf1b0fb66581218a3
emyarod <afw5059@gmail.com>
2020-06-02 23:55:14 -0500

    add translation TTS

--- src/_commands/translate.ts
+++ src/_commands/translate.ts
@@ -1,11 +1,19 @@
-import { Message } from 'discord.js';
+import { Message, MessageAttachment } from 'discord.js';
 import fetch from 'node-fetch';
 import { v4 as uuidv4 } from 'uuid';
+import { create } from 'xmlbuilder2';
 import {
+  msCognitiveSpeechTTSSubscriptionKey,
   msTranslatorSubscriptionKey,
   defaultTranslationOutputLanguage,
 } from '../../cfg/opendoors';
 import { URLSearchParams } from 'url';
+
+if (!msCognitiveSpeechTTSSubscriptionKey) {
+  throw new Error(
+    'Environment variable for your Azure subscription key for Cognitive Speech TTS is not set!'
+  );
+}
 
 if (!msTranslatorSubscriptionKey) {
   throw new Error(
@@ -27,10 +35,61 @@ async function fetchLanguages() {
     .then(({ translation }) => translation);
 }
 
+async function fetchVoices(subscriptionKey: string) {
+  return fetch(
+    'https://westus.tts.speech.microsoft.com/cognitiveservices/voices/list',
+    {
+      method: 'GET',
+      headers: {
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+      },
+    }
+  )
+    .then(res => res.json())
+    .then(data =>
+      data.reduce(
+        (
+          voicesObj: object,
+          {
+            Locale,
+            ShortName: shortName,
+          }: { Locale: string; ShortName: string }
+        ) => {
+          const [languageCode] = Locale.split('-');
+          voicesObj[languageCode] = voicesObj[languageCode]
+            ? voicesObj[languageCode].concat(shortName)
+            : [shortName];
+
+          return voicesObj;
+        },
+        {}
+      )
+    );
+}
+
+function getTTSAccessToken(subscriptionKey: string) {
+  return fetch(
+    'https://westus.api.cognitive.microsoft.com/sts/v1.0/issuetoken',
+    {
+      method: 'POST',
+      headers: {
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+      },
+    }
+  )
+    .then(res => res.text())
+    .catch(console.error);
+}
+
 // TODO: replace with top level await when it is released
 let languages = {};
+let voices: object | object[];
+let TTSAccessToken: string = '';
 (async () => {
   languages = await fetchLanguages();
+  voices = await fetchVoices(msCognitiveSpeechTTSSubscriptionKey);
+  TTSAccessToken =
+    (await getTTSAccessToken(msCognitiveSpeechTTSSubscriptionKey)) || '';
 })();
 
 function translate({
@@ -64,6 +123,50 @@ function translate({
     .catch(console.error);
 }
 
+async function textToSpeech({
+  accessToken,
+  languageCode,
+  text,
+}: {
+  accessToken: string;
+  languageCode: string;
+  text: string;
+}) {
+  const voicesList = voices[languageCode];
+  if (!voicesList) {
+    return null;
+  }
+  // Create the SSML request.
+  const xmlBody = create()
+    .ele('speak')
+    .att('version', '1.0')
+    .att('xml:lang', 'en-us')
+    .ele('voice')
+    .att('xml:lang', 'en-us')
+    // pick a random voice
+    .att('name', voicesList[Math.floor(Math.random() * voicesList.length)])
+    .txt(text)
+    .end();
+  // Convert the XML into a string to send in the TTS request.
+  const body = xmlBody.toString();
+  return fetch('https://westus.tts.speech.microsoft.com/cognitiveservices/v1', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'cache-control': 'no-cache',
+      'User-Agent': 'DISCORCABOT',
+      'X-Microsoft-OutputFormat': 'audio-24khz-160kbitrate-mono-mp3',
+      'Content-Type': 'application/ssml+xml',
+    },
+    body,
+  }).then(res => {
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    return res.buffer();
+  });
+}
+
 export default {
   name: 'translate',
   description: `Translate text from one language to another`,
@@ -73,6 +176,13 @@ export default {
   execute: async (message: Message, args: string[]) => {
     if (!languages[defaultTranslationOutputLanguage]) {
       languages = await fetchLanguages();
+    }
+    if (!voices) {
+      voices = await fetchVoices(msCognitiveSpeechTTSSubscriptionKey);
+    }
+    if (!TTSAccessToken) {
+      TTSAccessToken =
+        (await getTTSAccessToken(msCognitiveSpeechTTSSubscriptionKey)) || '';
     }
     try {
       const [from, to] = args[0]?.split(':') ?? [null, null];
@@ -106,9 +216,15 @@ export default {
         languages[translatorOutput?.detectedLanguage?.language || from].name;
       const outputLanguage =
         languages[to || defaultTranslationOutputLanguage].name;
-      return message.channel.send(
-        `${inputLanguage} to ${outputLanguage} translation: ${translatedText}`
-      );
+      const audioBuffer = await textToSpeech({
+        accessToken: TTSAccessToken,
+        languageCode: to || defaultTranslationOutputLanguage,
+        text: translatedText,
+      });
+      const messageText = `${inputLanguage} to ${outputLanguage} translation: ${translatedText}`;
+      const filename = `${translatedText.split(' ').join('_')}.wav`;
+      const attachment = new MessageAttachment(audioBuffer, filename);
+      return message.channel.send(messageText, attachment);
     } catch (error) {
       console.error(error, 'There was an error with your translation!');
       return message.reply('There was an error with your translation!');
